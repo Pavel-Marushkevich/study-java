a3fc0248fbf31a413596fa283f1d69d140e0744e
Pavel-Marushkevich <p.marushkevich@gmail.com>
2025-03-13 20:07:24 +0300

    UPD01

---
+++ test.java.classes/src/com/edu/les02/hw/main/ConsProg.java
@@ -0,0 +1,34 @@
+package com.edu.les02.hw.main;
+
+public class ConsProg {
+    public static void main(String[] args) {
+
+        System.out.println("Task01:\n");
+
+            int x;
+            int y;
+            int sum;
+            x = 11;
+            y = 9;
+            sum = x + y;
+            System.out.println("x="+sum+"\n");
+
+        System.out.println("Task02:\n");
+
+
+
+        System.out.println("Task03:\n");
+
+
+
+        System.out.println("Task04:\n");
+
+
+
+        System.out.println("Task05:\n");
+
+
+
+
+    }
+}
